1ae6ae7e7e23d0942be1882ebc7e49a036334e34
john38617 <157109748+john38617@users.noreply.github.com>
2024-02-23 17:08:32 +0800

    Create PasswordGenerator.py

---
+++ PasswordGenerator.py
@@ -0,0 +1,29 @@
+"""
+Program:
+    Generates Random Passwords based on the Length the User's Desire.
+"""
+
+# import random, import string
+import random
+import string
+
+# create string of random characters
+PasswordOriginal = string.ascii_letters+string.digits+string.punctuation
+# convert string into list
+PasswordList = list(PasswordOriginal)
+
+# shuffle list, convert list into string
+random.shuffle(PasswordList)
+PasswordNew = ''.join(PasswordList)
+
+# create the Index Length of Character of Password
+IndexLength = int(len(PasswordNew))-1
+
+# ask the user the amount of characters the password will be
+Amount = int(input("Length of Password (max:94chars): "))
+
+# limit how many characters the user wants using indexing starting from 0
+LimitPassword = PasswordNew[:Amount]
+
+# output the information
+print(LimitPassword)
